1caf93331a31323a521e6b3eb2dd7c4724ddb2fb
Ali AKAN <aali.akan@gmail.com>
2023-01-04 19:36:30 +0300

    first commit

---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,13 @@
+import classes from "./TodoItem.module.css";
+
+const TodoITem: React.FC<{ text: string; onRemoveTodo: () => void }> = (
+  props
+) => {
+  return (
+    <li className={classes.item} onClick={props.onRemoveTodo}>
+      {props.text}
+    </li>
+  );
+};
+
+export default TodoITem;
---
+++ src/components/Todos.tsx
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import TodoITem from "./TodoItem";
+import classes from "./Todos.module.css";
+import { TodosContext } from "../store/todos-context";
+
+const Todos: React.FC = () => {
+  const todosCtx = useContext(TodosContext);
+  return (
+    <ul className={classes.todos}>
+      {todosCtx.items.map((item) => (
+        <TodoITem
+          key={item.id}
+          text={item.text}
+          onRemoveTodo={todosCtx.removeTodo.bind(null, item.id)}
+        />
+      ))}
+    </ul>
+  );
+};
+export default Todos;
